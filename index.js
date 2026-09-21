a058c3624af04e7fc099f3acb75289217f85b0e4
mahmudratul <mahmudratul46@gmail.com>
2026-09-21 19:34:42 +0600

    layout

---
+++ index.js
@@ -0,0 +1,7 @@
+let heartCount = 0;
+let coinCount = 100;
+let copyCount = 0;
+// Navbar DOM references
+const heartCountEl = document.getElementById("heart-count");
+const coinCountEl = document.getElementById("coin-count");
+const copyCountEl = document.getElementById("copy-count");
